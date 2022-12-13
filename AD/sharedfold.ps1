fd3c16d93df01fae5516c27e84934bcef53ef97e
Asarck <85520089+Asarck@users.noreply.github.com>
2022-12-13 14:11:35 +0100

    a

--- AD/sharedfold.ps1
+++ AD/sharedfold.ps1
@@ -5,5 +5,5 @@ $folders | %{
 	mkdir "c:\$ou"
 	New-SmbShare -name $ou -Path "c:\$ou" -FullAccess "CESI\alede"
 	New-PSDrive -Name $ou -Root "\\SRVADDS.cesi.local\$ou" -PSProvider FileSystem -Persist
-	New-SmbGlobalMapping -LocalPath $("$A"+":") -RemotePath "\\LAB-DC1\$ou" -FullAccess "CESI\$ou" -Persistent $true
+	New-SmbGlobalMapping -LocalPath $("$A"+":") -RemotePath "\\LAB-DC1\$ou" -FullAccess "CESI\$ou" -Persistent $true -Credential
 }
